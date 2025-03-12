702c85784988887171c00e78500778effb7dee98
hwangsungyo <gdkds12@gmail.com>
2025-03-12 22:17:17 +0900

    FIx0

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -5,14 +5,16 @@ import { IoArrowForward } from "react-icons/io5";
 
 export const Section = () => {
   return (
-    <div className="flex w-full h-[1200px] items-end justify-center px-14 py-0 relative overflow-hidden">
+    <div className="flex w-full min-h-screen items-center justify-center px-14 py-0 relative overflow-hidden">
       <div
         className="absolute w-full h-full top-0 left-0 bg-cover bg-center"
-        style={{ backgroundImage: "url(/images/main-home.jpg)" }} // 이미지 경로 수정
+        style={{
+          backgroundImage: "url(/images/main-home.jpg)",
+        }}
       />
 
-      {/* 이 div에 z-index가 있는지 확인 */}
-      <div className="max-w-[1440px] h-[1041px] gap-8 pt-0 pb-[150px] px-0 flex-1 flex flex-col items-start relative z-10">
+      {/* z-index 확인 및 내용 div */}
+      <div className="max-w-[1440px]  gap-16 py-[15vh] px-0 flex-1 flex flex-col items-start relative z-10"> {/* Changed here */}
         <div className="self-stretch w-full flex flex-col items-start">
           <div className="self-stretch text-white text-[65px] leading-[85px] font-medium tracking-[-0.32px]">
             진우교회에 오신
